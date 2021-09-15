3cffe186c1790d9e25f4a62a038efd5e3e682430
saurabhparmar08 <42743557+saurabhparmar08@users.noreply.github.com>
2021-09-15 20:40:07 +0530

    Initial commit via file upload

---
+++ precache-manifest.501142c62f7e702b06d9eee67b55790d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d22c177ffdb12f2145d0574d6183b52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71f5617e355595838f27",
+    "url": "/static/css/2.844aa0ee.chunk.css"
+  },
+  {
+    "revision": "bf9074dcc2eee8c75cb4",
+    "url": "/static/css/main.6ecb433f.chunk.css"
+  },
+  {
+    "revision": "71f5617e355595838f27",
+    "url": "/static/js/2.4d13d594.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4d13d594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9074dcc2eee8c75cb4",
+    "url": "/static/js/main.204bf874.chunk.js"
+  },
+  {
+    "revision": "212a6ba042b27bf29c30",
+    "url": "/static/js/runtime-main.716769f5.js"
+  }
+]);
